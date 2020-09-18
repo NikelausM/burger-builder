088ecd699f6800a209eb448e7faedd1e7d0f27bd
NikelausM <jnickm@gmail.com>
2020-09-18 13:32:40 -0600

    Add basic static burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,12 +4,13 @@ import React,
 } from 'react'
 
 import Aux from 'hoc/Auxiliary'
+import Burger from 'components/Burger/Burger'
 
 class BurgerBuilder extends Component {
   render() {
     return (
       <Aux>
-        <div>Burger</div>
+        <Burger />
         <div>Build Controls</div>
       </Aux>
     )
