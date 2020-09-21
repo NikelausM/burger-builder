9e1d0df66e35e4db00b1d6a92aa41f1e2969cf0e
NikelausM <jnickm@gmail.com>
2020-09-21 10:19:36 -0600

    Implement buttons and add total price

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Aux from 'hoc/Auxiliary'
+import Button from 'components/UI/Button/Button'
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients)
@@ -19,9 +20,16 @@ const orderSummary = (props) => {
       <ul>
         {ingredientSummary}
       </ul>
+      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
       <p>Continue to Checkout?</p>
-      <button>CANCEL</button>
-      <button>CONTINUE</button>
+      <Button
+        btnType="Danger"
+        clicked={props.purchaseCancelled}
+      >CANCEL</Button>
+      <Button
+        btnType="Success"
+        clicked={props.purchaseContinued}
+      >CONTINUE</Button>
     </Aux>
   )
 }
