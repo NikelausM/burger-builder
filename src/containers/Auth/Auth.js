a7cf7c5d6adbdf63a22e7d9321db68174ba3edf9
NikelausM <jnickm@gmail.com>
2020-10-16 16:25:07 -0600

    Add redirection during build burger process

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 
 import Input from 'components/UI/Input/Input'
 import Button from 'components/UI/Button/Button'
@@ -42,6 +43,12 @@ class Auth extends Component {
       },
     },
     isSignup: true
+  }
+
+  componentDidMount() {
+    if (!this.props.buildingBurger && this.props.authRedirectPath !== '/') {
+      this.props.onSetAuthRedirectPath()
+    }
   }
 
   checkValidity = (value, rules) => {
@@ -126,8 +133,13 @@ class Auth extends Component {
       <p>{this.props.error.message}</p>
     ) : null
 
+    const authRedirect = this.props.isAuthenticated ? (
+      <Redirect to={this.props.authRedirectPath} />
+    ) : null
+
     return (
       <div className={classes.Auth}>
+        {authRedirect}
         {errorMessage}
         <form onSubmit={this.submitHandler}>
           {form}
@@ -144,13 +156,17 @@ class Auth extends Component {
 const mapStateToProps = state => {
   return {
     loading: state.auth.loading,
-    error: state.auth.error
+    error: state.auth.error,
+    isAuthenticated: state.auth.token !== null,
+    buildingBurger: state.burgerBuilder.building,
+    authRedirectPath: state.auth.authRedirectPath
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
+    onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup)),
+    onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/'))
   }
 }
 
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -6,7 +6,8 @@ const initialState = {
   token: null,
   userId: null,
   error: null,
-  loading: false
+  loading: false,
+  authRedirectPath: '/'
 }
 
 const authStart = (state, action) => {
@@ -18,7 +19,7 @@ const authSuccess = (state, action) => {
     token: action.idToken,
     userId: action.userId,
     error: null,
-    loading: false
+    loading: false,
   })
 }
 
@@ -33,6 +34,10 @@ const authLogout = (state, action) => {
   return updateObject(state, { token: null, userId: null })
 }
 
+const setAuthRedirectPath = (state, action) => {
+  return updateObject(state, { authRedirectPath: action.path })
+}
+
 const reducer = (state = initialState, action) => {
   try {
     switch (action.type) {
@@ -40,6 +45,7 @@ const reducer = (state = initialState, action) => {
       case actionTypes.AUTH_SUCCESS: return authSuccess(state, action)
       case actionTypes.AUTH_FAIL: return authFail(state, action)
       case actionTypes.AUTH_LOGOUT: return authLogout(state, action)
+      case actionTypes.SET_AUTH_REDIRECT_PATH: return setAuthRedirectPath(state, action)
       default: return state
     }
   } catch (error) {
